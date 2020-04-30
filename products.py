1bd8e376924ee2650874a77cd3b8a2f19cc99efe
Sunny Choi <yukhengchoi@gmail.com>
2020-04-30 23:59:36 +0800

    add read file

--- products.py
+++ products.py
@@ -1,16 +1,27 @@
+# read file
 products = []
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if 'Product,Price' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
+# enter notes by user
 while True:
 	name = input('Please enter the products: ')
 	if name == 'q': # quit
 		break
 	price = input('Please enter the price: $')
 	products.append([name, price])
-	
 print(products)
 
+# print all records
 for p in products:
 	print('The price of', p[0], 'is $', p[1])
 
+# write file
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('Product,Price\n')
 	for p in products:
