f8a154ab5a7b437645661893e85a1e1c0965d170
Sunny Choi <yukhengchoi@gmail.com>
2020-05-03 04:30:55 +0800

    refactor

--- products.py
+++ products.py
@@ -1,35 +1,48 @@
 import os # operating system
 
-# read file or creat file
-products = []
-if os.path.isfile('products.csv'): # 相對路徑 # os is a module, path is another module
-	print('Yeah! We found it!')
-	with open('products.csv', 'r', encoding='utf-8') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
 			if 'Product,Price' in line:
 				continue
 			name, price = line.strip().split(',')
 			products.append([name, price])
-	print(products)
-
-else:
-	print('We cannot find it......')
+	return products
 
 # enter notes by user
-while True:
-	name = input('Please enter the products: ')
-	if name == 'q': # quit
-		break
-	price = input('Please enter the price: $')
-	products.append([name, price])
-print(products)
+def user_input(products):
+	while True:
+		name = input('Please enter the products: ')
+		if name == 'q': # quit
+			break
+		price = input('Please enter the price: $')
+		products.append([name, price])
+	print(products)
+	return products
 
 # print all records
-for p in products:
-	print('The price of', p[0], 'is $', p[1])
+def print_products(products):
+	for p in products:
+		print('The price of', p[0], 'is $', p[1])
 
 # write file
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('Product,Price\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+def write_file(filename, products):
+	with open(filename, 'w', encoding='utf-8') as f:
+		f.write('Product,Price\n')
+		for p in products:
+			f.write(p[0] + ',' + p[1] + '\n')
+
+def main(): # command all = main function
+	filename = 'products.csv'
+	if os.path.isfile(filename): # 相對路徑 # os is a module, path is another module
+		print('Yeah! We found it!')
+		products = read_file(filename)
+	else:
+		print('We cannot find it......')
+
+	products = user_input(products)
+	print_products(products)
+	write_file('products.csv', products)
+
+main() # after refactor 
